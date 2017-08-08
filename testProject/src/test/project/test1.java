e4229b70a3908b04f82c9379749d6aa24b2d66fa
Annie <Annie@Annie-PC.home>
2017-08-08 14:17:11 -0400

    TestProject new

---
+++ testProject/src/test/project/test1.java
@@ -0,0 +1,14 @@
+package test.project;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+	ChromeDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+		
+	}
+
+}
